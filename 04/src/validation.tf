543f5c0dfc5d64ef02efe53e68952d689c99cb18
Zolotkoff <-email@-.com>
2026-07-15 22:35:03 +0300

    terraform-05: add IP validation variables (task 4)

---
+++ 04/src/validation.tf
@@ -0,0 +1,21 @@
+variable "ip_address" {
+  type        = string
+  description = "ip-адрес"
+  default     = "192.168.0.1"
+
+  validation {
+    condition     = can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.ip_address))
+    error_message = "Invalid IP address format."
+  }
+}
+
+variable "ip_address_list" {
+  type        = list(string)
+  description = "список ip-адресов"
+  default     = ["192.168.0.1", "1.1.1.1", "127.0.0.1"]
+
+  validation {
+    condition     = alltrue([for ip in var.ip_address_list : can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip))])
+    error_message = "All elements must be valid IP addresses."
+  }
+}
